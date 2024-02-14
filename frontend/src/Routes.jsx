a2d37e307b53cfa55145a735ee757e1621531195
ansengarvin <45224464+ansengarvin@users.noreply.github.com>
2024-02-14 10:15:51 -0800

    Added boilerplate front end code

---
+++ frontend/src/Routes.jsx
@@ -0,0 +1,11 @@
+import { useState } from 'react'
+import {topbar} from './components/topbar/topbar'
+
+export function Root(props) {
+  const {children} = props
+  return (
+    <>
+      Hello World
+    </>
+  )
+}
---
+++ frontend/src/components/topbar/topbar.jsx
@@ -0,0 +1,9 @@
+﻿import React from 'react'
+
+export function topbar() {
+    return (
+        <nav class="topbar">
+            Top Bar
+        </nav>
+    )
+}
